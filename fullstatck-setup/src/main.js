d6dbcb181497f5142ba5da7dfe97b6f3c3c74b8f
shashank.v.shandilya@gmail.com <admin@spurath.com>
2018-12-15 12:15:39 +0530

    added sass

--- fullstatck-setup/src/main.js
+++ fullstatck-setup/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+
 import App from './App.vue';
+
 import router from './router';
+
 import store from './store';
+
+import './assets/scss/main.scss';
 
 Vue.config.productionTip = false;
 
